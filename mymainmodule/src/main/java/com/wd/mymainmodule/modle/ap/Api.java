04d2d67cb30c8a3b3944ad137dcb88a13fcde6ed
yue <he@999.com>
2019-12-13 21:54:50 +0800

    登录注册

--- mymainmodule/src/main/java/com/wd/mymainmodule/modle/ap/Api.java
+++ mymainmodule/src/main/java/com/wd/mymainmodule/modle/ap/Api.java
@@ -1,11 +1,17 @@
 package com.wd.mymainmodule.modle.ap;
 
 import com.wd.mymainmodule.modle.bean.LoginBean;
+import com.wd.mymainmodule.modle.bean.RechargeBean;
+import com.wd.mymainmodule.modle.bean.UserBean;
+
+import java.util.Map;
 
 import io.reactivex.Observable;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * date:2019/12/12
@@ -13,7 +19,14 @@ import retrofit2.http.POST;
  * function:
  */
 public interface Api {
-    @FormUrlEncoded
+//  登录
     @POST("health/user/v1/login")
-    Observable<LoginBean> doLogin(@Field("email") String email, @Field("pwd") String pwd);
+    Observable<LoginBean> doLogin(@Query("email") String email, @Query("pwd") String pwd);
+//  发送验证码
+    @POST("health/user/v1/sendOutEmailCode")
+    Observable<UserBean> doEmailCode(@Query("email") String email);
+//  注册
+    @POST("health/user/v1/register")
+    Observable<UserBean> doRegister(@QueryMap Map<String,Object> map);
+
 }
