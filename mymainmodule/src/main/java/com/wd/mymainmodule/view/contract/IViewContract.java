2e4fbd2999d47ad93f74d900f6437d377038426c
yue <he@999.com>
2019-12-13 13:44:56 +0800

    mvpz添加

--- mymainmodule/src/main/java/com/wd/mymainmodule/view/contract/IViewContract.java
+++ mymainmodule/src/main/java/com/wd/mymainmodule/view/contract/IViewContract.java
@@ -1,10 +1,6 @@
 package com.wd.mymainmodule.view.contract;
 
-import com.bwie.mvplibrary.base.BasePresenter;
 import com.bwie.mvplibrary.base.IBaseView;
-import com.wd.mymainmodule.view.IBasesView;
-
-import java.util.Map;
 
 /**
  * date:2019/12/13
@@ -12,5 +8,32 @@ import java.util.Map;
  * function: 契约类
  */
 public interface IViewContract {
+    //V层
+    interface IView extends IBaseView {
+        void onSuccess(Object obj);
+        void onSuccessOne(Object one);
+        void onSuccessTwo(Object two);
+        void onSuccessThree(Object three);
+        void onSuccessFour(Object four);
+        void onFail(String str);
+    }
 
+    //P层
+    interface IPresenter {
+        //登录
+        void doLogin(String email,String pwd);
+    }
+
+    //M层
+    interface IModel{
+        void doLogin(String email,String pwd,IModelCallback iModelCallback);
+        interface IModelCallback{
+            void onSuccess(Object obj);
+            void onSuccessOne(Object one);
+            void onSuccessTwo(Object two);
+            void onSuccessThree(Object three);
+            void onSuccessFour(Object four);
+            void onFail(String str);
+        }
+    }
 }
