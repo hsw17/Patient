71756e136178bcccd43c7659e50e5e23860bf7b9
yue <he@999.com>
2019-12-15 20:57:57 +0800

    添加frace

--- mvplibrary/src/main/java/com/bwie/mvplibrary/app/App.java
+++ mvplibrary/src/main/java/com/bwie/mvplibrary/app/App.java
@@ -3,6 +3,7 @@ package com.bwie.mvplibrary.app;
 import android.app.Application;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 
 /*
@@ -20,6 +21,7 @@ public class App extends Application {
         ARouter.openDebug();
         ARouter.init(this);
         sContent = this;
+        Fresco.initialize(this);
     }
 
     public static App getsContent() {
--- mymodlue/src/main/java/com/wd/mymodlue/view/activity/MainActivity.java
+++ mymodlue/src/main/java/com/wd/mymodlue/view/activity/MainActivity.java
@@ -1,16 +1,52 @@
 package com.wd.mymodlue.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
+import com.bwie.mvplibrary.base.BaseActivity;
 import com.wd.mymodlue.R;
+import com.wd.mymodlue.persenter.Persenter;
+import com.wd.mymodlue.view.contract.IViewContract;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity<Persenter> implements IViewContract.IView {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int bindLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected Persenter setPresenter() {
+        return new Persenter();
+    }
+
+    @Override
+    public void onSuccess(Object obj) {
+
+    }
+
+    @Override
+    public void onSuccessOne(Object one) {
+
+    }
+
+    @Override
+    public void onSuccessTwo(Object two) {
+
+    }
+
+    @Override
+    public void onSuccessThree(Object three) {
+
+    }
+
+    @Override
+    public void onSuccessFour(Object four) {
+
+    }
+
+    @Override
+    public void onFail(String str) {
+
     }
 }
