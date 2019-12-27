5c32e70a5eb4683acfc4ff9440054c0472cb28bf
yue <he@999.com>
2019-12-27 11:46:29 +0800

    合并 张

--- mvplibrary/src/main/java/com/bwie/mvplibrary/app/App.java
+++ mvplibrary/src/main/java/com/bwie/mvplibrary/app/App.java
@@ -8,10 +8,7 @@ import android.content.IntentFilter;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.tencent.mm.opensdk.constants.ConstantsAPI;
-import com.tencent.mm.opensdk.openapi.IWXAPI;
-import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
 
 
 /*
@@ -24,8 +21,6 @@ public class App extends Application {
     // APP_ID 替换为你的应用从官方网站申请到的合法appID
     private static final String APP_ID = "wxe3fcbe8a55cd33ff";
 
-    // IWXAPI 是第三方app和微信通信的openApi接口
-    public static IWXAPI api;
     @Override
     public void onCreate() {
         super.onCreate ();
@@ -34,29 +29,8 @@ public class App extends Application {
         ARouter.init(this);
         sContent = this;
         Fresco.initialize(this);
-
-        regToWx();
-        ZXingLibrary.initDisplayOpinion ( this );
-
-
     }
-    private void regToWx() {
-        // 通过WXAPIFactory工厂，获取IWXAPI的实例
-        api = WXAPIFactory.createWXAPI(this, APP_ID, true);
 
-        // 将应用的appId注册到微信
-        api.registerApp(APP_ID);
-
-        //建议动态监听微信启动广播进行注册到微信
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-
-                // 将该app注册到微信
-                api.registerApp(APP_ID);
-            }
-        }, new IntentFilter(ConstantsAPI.ACTION_REFRESH_WXAPP));
-    }
     public static App getsContent() {
         return sContent;
     }
--- mvplibrary/src/main/java/com/bwie/mvplibrary/utils/IWXAPIEventHandler.java
+++
@@ -1,9 +0,0 @@
-package com.bwie.mvplibrary.utils;
-
-/**
- * date:2019/12/27
- * author:贺少伟(盗)
- * function:
- */
-interface IWXAPIEventHandler {
-}
